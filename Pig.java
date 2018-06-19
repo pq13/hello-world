0ba8d7d57322689a87134203a95414b405ab09ec
Malignus <and16787528@yandex.ru>
2018-06-19 19:24:56 +0300

    Added Pig.java

---
+++ Pig.java
@@ -0,0 +1,9 @@
+public class Pig extends Animal {
+	public Pig(String name, int weight, int age){
+		super(name, weight ,age);
+		System.out.println("Я свинюшка!");
+	}
+	public void say() {
+	System.out.println("Хрю-хрю");
+	}
+}
