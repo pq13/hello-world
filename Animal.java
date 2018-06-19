69d86152fd4537334dd0c5a4b94028ce429af698
Malignus <and16787528@yandex.ru>
2018-06-19 18:06:06 +0300

    Added Animal.java

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+	String name;
+	int age;
+	float weight;
+	public Animal(String name, int age, int weight) {
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+		System.out.println("Я родилось");
+	}		
+}
