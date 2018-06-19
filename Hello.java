380ab4792f24d7dfa89c41e786b115d05be33d90
Malignus <and16787528@yandex.ru>
2018-06-19 20:55:53 +0300

    Some changes

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Привет жоппа!");
+		System.out.println("Привет жоппа! Создадим животное и свинку");
+		Animal animal = new Animal("Alice", 5, 6);
+		Pig pig = new Pig("Хрюня", 2, 30);
+		pig.say();
 	}
 }
